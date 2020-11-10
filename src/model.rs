5c60c2bf97533ae85f65ac761a777b1a235ac0f4
syariefa <syareeefa@gmail.com>
2020-11-10 23:31:11 +0700

    buat model pendukung response dan body request

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,19 @@
 use std::num::NonZeroU16;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct BodyReq {
     pub denom: NonZeroU16
+}
+
+#[derive(Debug, Serialize)]
+pub struct BodyResp {
+    pub op: String,
+    pub result: u16
+}
+
+#[derive(Debug, Serialize)]
+pub struct ErrorMessage {
+    pub code: u16,
+    pub message: String
 }
